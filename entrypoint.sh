6185e4181656ce175abc83f6fd63b77939acebe9
ankitjain28may <ankitjain28may77@gmail.com>
2019-11-12 02:16:33 +0530

    Passing env

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-/usr/bin/curl -X POST https://api.twilio.com/2010-04-01/Accounts/$INPUT_SID/Messages.json \
+/usr/bin/curl -X POST https://api.twilio.com/2010-04-01/Accounts/$SID/Messages.json \
     --data-urlencode "From=$INPUT_FROM" \
     --data-urlencode "Body=$INPUT_BODY" \
     --data-urlencode "To=$INPUT_TO" \
-    -u $INPUT_SID:$INPUT_AUTHTOKEN > result.json
+    -u $INPUT_SID:$AUTH_TOKEN > result.json
 
 echo ::set-output name=status::`cat result.json | /usr/bin/jq '.status'`
